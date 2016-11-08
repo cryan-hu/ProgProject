924bb106007bad9220023417ac58bddddeed238e
Jorrit Strikwerda <jorrit.strikwerda@student.hu.nl>
2016-11-08 22:05:12 +0100

    no message

--- Python/apicsv.py
+++ Python/apicsv.py
@@ -1,12 +1,23 @@
+import csv
+import requests
+import xmltodict
+
+def apicall(url):
+    inloggegevens = 'jorrit.strikwerda@student.hu.nl','z0iqfPJWHGLQq-ayz7TBQyp22imLNmKZpuLidpH8HHIdT-I04zBV7g'
+    response = requests.get(url, auth=inloggegevens)
+    vertrekXML = xmltodict.parse(response.text)
+    return vertrekXML
+
 def stations():
-    with open('stations.csv', 'w', newline='') as bestand:
+    with open('stations.csv', 'w', newline='', encoding='UTF-8') as bestand:
         schrijf = csv.writer(bestand, delimiter=';')
-        schrijf.writerow(('code','naamKort','naamMiddel','naamLang','lat','lon'))
+        schrijf.writerow(('code','naamLang','lat','lon'))
         for station in apicall('http://webservices.ns.nl/ns-api-stations-v2')['Stations']['Station']:
             code = station['Code']
             naamLang = station['Namen']['Lang']
-            naamMiddel = station['Namen']['Middel']
-            naamKort = station['Namen']['Kort']
             lat = station['Lat']
             lon = station['Lon']
-            schrijf.writerow((code,naamKort,naamMiddel,naamLang,lat,lon))
+            schrijf.writerow((code,naamLang,lat,lon))
+
+stations()
+
