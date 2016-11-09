43b8e78ad404edb025d49fa9d22af18438ed6f48
Jorrit Strikwerda <jorrit.strikwerda@student.hu.nl>
2016-11-09 19:04:47 +0100

    meer stuff

--- Python/apivertrektijdenenstations.py
+++ Python/apivertrektijdenenstations.py
@@ -1,6 +1,7 @@
 import requests
 import xmltodict
 
+
 locatieAutomaat = "ut"
 def apicall(url):
     inloggegevens = 'jorrit.strikwerda@student.hu.nl','z0iqfPJWHGLQq-ayz7TBQyp22imLNmKZpuLidpH8HHIdT-I04zBV7g'
@@ -8,7 +9,13 @@ def apicall(url):
     vertrekXML = xmltodict.parse(response.text)
     return vertrekXML
 
-def vertrekUt():
+def werkzaamheden(): #werkt niet
+    print('Dit zijn de werkzaamheden en storingen:')
+    for werk in apicall('https://webservices.ns.nl/ns-api-storingen?station='+locatieAutomaat)['Storingen']['Ongepland']['Storing']:
+        traject = werk['Traject'] #er gaat hier iets mis -> hij ziet werk als string en niet als dict, hoe komt dit?
+        print(traject)
+
+def vertrekUt(): #werkt wel
     print('Dit zijn de vertrekkende treinen:')
     for vertrek in apicall('https://webservices.ns.nl/ns-api-avt?station='+locatieAutomaat)['ActueleVertrekTijden']['VertrekkendeTrein']:
         eindbestemming = vertrek['EindBestemming']
@@ -22,16 +29,7 @@ def vertrekUt():
         elif spoorWijziging == 'true':
             gewijzigd = 'Wel'
         treinsoort = vertrek['TreinSoort']
-
         print('Om {} vertrekt een {} naar {} van spoor {}({} gewijzigd).'.format(vertrektijd,treinsoort,eindbestemming,spoor,gewijzigd))
 
-def stations():
-    for station in apicall('http://webservices.ns.nl/ns-api-stations-v2')['Stations']['Station']:
-        code = station['Code']
-        naamLang = station['Namen']['Lang']
-        lat = station['Lat']
-        lon = station['Lon']
-        print('{} - {} - {} - {}'.format(code,naamLang,lat,lon))
 
-
-vertrekUt()
+werkzaamheden()
