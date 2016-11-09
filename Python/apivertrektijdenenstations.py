82cf0fe5f3ca82b1f62527a0120d282241b123d3
Jorrit Strikwerda <jorrit.strikwerda@student.hu.nl>
2016-11-09 16:52:15 +0100

    spoor(gewijzigd) en soort trein toegevoegd

--- Python/apivertrektijdenenstations.py
+++ Python/apivertrektijdenenstations.py
@@ -1,7 +1,7 @@
 import requests
 import xmltodict
 
-locatieAutomaat = "br"
+locatieAutomaat = "ut"
 def apicall(url):
     inloggegevens = 'jorrit.strikwerda@student.hu.nl','z0iqfPJWHGLQq-ayz7TBQyp22imLNmKZpuLidpH8HHIdT-I04zBV7g'
     response = requests.get(url, auth=inloggegevens)
@@ -12,9 +12,18 @@ def vertrekUt():
     print('Dit zijn de vertrekkende treinen:')
     for vertrek in apicall('https://webservices.ns.nl/ns-api-avt?station='+locatieAutomaat)['ActueleVertrekTijden']['VertrekkendeTrein']:
         eindbestemming = vertrek['EindBestemming']
-        vertrektijd = vertrek['VertrekTijd'] # 2016-09-27T18:36:00+0200
-        vertrektijd = vertrektijd[11:16] # 18:36
-        print('Om '+vertrektijd+' vertrekt een trein naar '+ eindbestemming)
+        vertrekTijdFull = vertrek['VertrekTijd'] # 2016-09-27T18:36:00+0200
+        vertrektijd = vertrekTijdFull[11:16] # 18:36
+        vertrekspoor = vertrek['VertrekSpoor']
+        spoor = vertrekspoor['#text']
+        spoorWijziging = vertrekspoor['@wijziging']
+        if spoorWijziging == 'false':
+            gewijzigd = 'Niet'
+        elif spoorWijziging == 'true':
+            gewijzigd = 'Wel'
+        treinsoort = vertrek['TreinSoort']
+
+        print('Om {} vertrekt een {} naar {} van spoor {}({} gewijzigd).'.format(vertrektijd,treinsoort,eindbestemming,spoor,gewijzigd))
 
 def stations():
     for station in apicall('http://webservices.ns.nl/ns-api-stations-v2')['Stations']['Station']:
